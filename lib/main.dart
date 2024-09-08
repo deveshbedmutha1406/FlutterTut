3c44fcde4312da852c2f7f24c7c9850df60ea14e
jagrutibedmutha <bedmuthajagruti17@gmail.com>
2024-09-08 12:51:43 +0530

    Intial Commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+
+class Home extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: const Center(
+        child: Text(
+            "Hello devesh",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: 'IndieFlower',
+            )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click"),
+        backgroundColor: Colors.red[600],
+      ),
+
+    );
+  }
+}
+
+
